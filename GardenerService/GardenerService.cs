4b5657f98125ea1e1b231fbe448fef414767555b
Vladimir Kotikov <v-vlkoti@microsoft.com>
2015-03-14 16:51:51 +0300

    Adds Idisposable to GardenerDevice, Fixes wrong servicepath param.

--- GardenerService/GardenerService.cs
+++ GardenerService/GardenerService.cs
@@ -3,40 +3,36 @@ using AllJoynUnity;
 
 namespace Gardener
 {
-    public class GardenerService
+    public class GardenerService :IDisposable
     {
         private const string SERVICE_NAME = "com.akvelon.gardener";
         private const string INTERFACE_NAME = "com.akvelon.gardener.flowerpot";
+        private const string SERVICE_PATH = "/flowerpot";
         
         private const bool ALLOW_REMOTE_MESSAGES = true;
-        private const AllJoyn.InterfaceDescription.SecurityPolicy DISABLE_SECURITY = AllJoyn.InterfaceDescription.SecurityPolicy.Off;
+        private const AllJoyn.InterfaceDescription.SecurityPolicy DISABLE_SECURITY =
+            AllJoyn.InterfaceDescription.SecurityPolicy.Off;
 
-        private ushort SERVICE_PORT = 1001;
-
-        private readonly IGardenerDevice activeDevice;
+        private readonly ushort SERVICE_PORT = 1001;
 
         private readonly AllJoyn.BusAttachment ajAttachment;
 
         public GardenerService(IGardenerDevice device)
         {
-            activeDevice = device;
             ajAttachment = new AllJoyn.BusAttachment("Gardener", ALLOW_REMOTE_MESSAGES);
-            
             AllJoyn.InterfaceDescription iface;
+
             ajAttachment.CreateInterface(INTERFACE_NAME, DISABLE_SECURITY, out iface);
 
             iface.AddProperty("humidity", "d", AllJoyn.InterfaceDescription.AccessFlags.Read);
             iface.AddProperty("solarFlow", "i", AllJoyn.InterfaceDescription.AccessFlags.Read);
-
             iface.AddMethod("waterPumpOn", "i", "b", "interval,hasActivator");
             iface.AddMethod("waterPumpOff", "i", "b", "interval,hasActivator");
-
             iface.Activate();
 
+            var busObject = new DeviceObject(ajAttachment, device, SERVICE_PATH, INTERFACE_NAME);
+
             ajAttachment.Start();
-
-            var busObject = new DeviceObject(ajAttachment, device, SERVICE_NAME, INTERFACE_NAME);
-
             ajAttachment.RegisterBusObject(busObject);
             ajAttachment.Connect();
 
@@ -48,7 +44,13 @@ namespace Gardener
                 AllJoyn.SessionOpts.ProximityType.Any, AllJoyn.TransportMask.Any);
 
             ajAttachment.BindSessionPort(ref SERVICE_PORT, sessionOpts, joinSessionListener);
+            
             ajAttachment.AdvertiseName(SERVICE_NAME, AllJoyn.TransportMask.Any);
+        }
+
+        public void Dispose()
+        {
+            ajAttachment.Dispose();
         }
     }
 
